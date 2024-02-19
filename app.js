ed400d998ff4a587be1a031c19072f0e29d86878
M'hamed El Mouahidi <amed@Mhameds-MBP.broadband>
2024-02-19 16:29:14 +0000

    GET /api/articles/:article_id completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const { getEndpoints } = require("./controllers/api.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 const { serverErrors, customErrors, psqlErrors  } = require("./controllers/errors.controllers");
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpoints);
+
+app.get('/api/articles/:article_id', getArticleById)
+
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
